f90b8b351c2473da454e2162272a6295006f5444
Engin Karataş <enginkaratas99@gmail.com>
2021-06-23 00:27:53 +0300

    folders added

---
+++ overloading/BaseCustomerManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FunctionOverloading
+{
+    public class BaseCustomerManager
+    {
+        public virtual void Add(Logger deger)
+        {
+            Console.WriteLine("base classtan " + deger + " eklendi");
+        }
+    }
+}
---
+++ overloading/CorparateCustomerManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionOverloading
+{
+    public class CorparateCustomerManager:BaseCustomerManager
+    {
+        public override void Add(Logger logger)
+        {
+            Console.WriteLine("logger geldi");
+        }
+    }
+}
