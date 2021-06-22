6c5fc12388f68e4a5f0d06463c6f31dbc5ee7ad8
Engin Karataş <enginkaratas99@gmail.com>
2021-06-23 00:24:48 +0300

    update folders

---
+++ delegate/delegeartieventhandler/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegeartieventhandler
+{
+    class Program
+    {
+        public delegate void benimdelegemEventHandler();
+        static void Main(string[] args)
+        {
+                 
+        }
+    }
+}
