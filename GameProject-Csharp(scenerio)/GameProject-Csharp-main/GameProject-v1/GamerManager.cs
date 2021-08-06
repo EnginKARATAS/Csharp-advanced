20cb767408114469196578cf426984dda9cf9a63
unknown <43602725+EnginKARATAS@users.noreply.github.com>
2021-08-06 21:34:26 +0300

    added GameProject-Csharp(scenerio) project demo

---
+++ GameProject-Csharp(scenerio)/GameProject-Csharp-main/GameProject-v1/GamerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject_v1
+{
+    class GamerManager:IEdevletService
+    {
+        public void Adduser()
+        {
+            CheckUserFromEdevlet();
+            Console.WriteLine("Game manager ile site kullanıcısı eklendi");
+        }
+
+        public void CheckUserFromEdevlet()
+        {
+            Console.WriteLine("e devletten kullanıcı kontrol edildi");
+        }
+
+
+    }
+}
---
+++ GameProject-Csharp(scenerio)/GameProject-Csharp-main/GameProject-v1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GameProject_v1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hi");
+            Console.ReadKey();
+        }
+    }
+}
